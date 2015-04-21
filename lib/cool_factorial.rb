bd24efcfdbe9552e9ac7ab1619edf1ec52adad33
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 16:23:02 -0700

    Refactor recursion. All specs pass

--- lib/cool_factorial.rb
+++ lib/cool_factorial.rb
@@ -1,9 +1,7 @@
 class Fixnum
   define_method(:cool_factorial) do
 
-    if(self.eql?(0))
-      1
-    elsif(self.eql?(1))
+    if(self.<=(1))
       1
     else
       self.-(1).cool_factorial.*(self)
