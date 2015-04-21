5a0fb179904c60d897a89d688fde9915d09d9856
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 15:07:15 -0700

    Initialize project directory and README

---
+++ README.md
@@ -0,0 +1,5 @@
+FACTORIAL
+
+A factorial is an operator that multiplies a number by all of the integers less than that number. For example, 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+By definition, 0! = 1.
---
+++ spec/cool_factorial_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('factorial')
+
+describe('Fixnum#factorial') do
+  it("Given a number, the method will return the factorial of that number") do
+    expect(1.factorial()).to(eq(1))
+  end
+end
