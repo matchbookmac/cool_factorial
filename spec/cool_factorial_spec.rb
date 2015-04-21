857ef4e3ba78202ea2be00e3eb53e06e7253256d
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 15:20:04 -0700

    Return correct value for all positive integers

--- spec/cool_factorial_spec.rb
+++ spec/cool_factorial_spec.rb
@@ -8,4 +8,7 @@ describe('Fixnum#cool_factorial') do
   it("Given a number, the method will return the factorial of that number") do
     expect(5.cool_factorial()).to(eq(120))
   end
+  it("Given a number, the method will return the factorial of that number") do
+    expect(10.cool_factorial()).to(eq(3628800))
+  end
 end
